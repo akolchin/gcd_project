127d81cd524b0513acdd74d5d0f1bd3da7fb1917
akolchin <akolchin@gmail.com>
2014-04-23 16:05:28 +0400

    partially implemented

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,38 @@
-## Merges the training and the test sets to create one data set.
+## load and unzip
 
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/data.zip")
+unzip("./data/data.zip", exdir="./data")
+
+## load and prepare datas
+activity_labels <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+colnames(activity_labels) <- c("id", "actname")
+
+features <- read.table("./data/UCI HAR Dataset/features.txt")
+colnames(features) <- c("column", "fname")
+
+## Merges the training and the test sets to create one data set.
+X <- rbind(read.table("./data/UCI HAR Dataset/test/X_test.txt"), read.table("./data/UCI HAR Dataset/train/X_train.txt"))
+colnames(X) <- features$fname
 
 ## Extracts only the measurements on the mean and standard deviation for each measurement. 
+extracted <- X[,grep("(-mean\\(\\)|-std\\(\\))", features$fname)]
 
+## further prepare datas
+y <- rbind(read.table("./data/UCI HAR Dataset/test/y_test.txt"), read.table("./data/UCI HAR Dataset/train/y_train.txt"))
+colnames(y) <- "label"
+
+subject <- rbind(read.table("./data/UCI HAR Dataset/test/subject_test.txt"), read.table("./data/UCI HAR Dataset/train/subject_train.txt"))
+colnames(subject) <- "subject"
+
+merged <- cbind(subject, y, extracted)
 
 ## Uses descriptive activity names to name the activities in the data set
-
-
 ## Appropriately labels the data set with descriptive activity names. 
-
+merged$label <- factor(merged$label, labels=activity_labels$actname)
 
 ## Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+## save it
+write.table(merged, file = "tidy.txt")
